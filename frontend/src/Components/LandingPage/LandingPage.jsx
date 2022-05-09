a6b46b8fcb9dd47fbd968a81b9fd523f52fd9d3f
Oghenetega Esedere <arcteggzz@gmail.com>
2022-05-09 23:11:04 +0000

    Started creating LandingPage components

---
+++ frontend/src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import Hero from "./Hero"
+
+function LandingPage(props){
+    
+    return(
+        <div className="h-full w-full">
+            <Hero/>
+        </div>
+    )
+}
+
+export default LandingPage;
---
+++ frontend/src/Components/Utility/Logo.jsx
@@ -0,0 +1,10 @@
+import logo from "../../Images/ARC803_Major Project_Presentation.png"
+
+function Logo(){
+    return(
+        <img className="lg:w-[256px] sm:w-[108px]" src={logo} alt="" />
+    )
+}
+
+export default Logo;
+//edit pocture to remove all whitespace
