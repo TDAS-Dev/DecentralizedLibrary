b1e1ff8d91667f2c931f8a182d4f904bd81061f5
Oghenetega Esedere <arcteggzz@gmail.com>
2022-05-16 21:16:27 +0100

    Styled the HomePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 // import LandingPage from "./Components/LandingPage/LandingPage";
-import AboutPage from "./Components/AboutPage/AboutPage";
+// import AboutPage from "./Components/AboutPage/AboutPage";
 // import UploadPage from "./Components/UploadPage/UploadPage";
-// import HomePage from "./Components/HomePage/HomePage";
+import HomePage from "./Components/HomePage/HomePage";
 
 function App() {
   return (
     <div className="App">
       {/* <LandingPage/> */}
-      <AboutPage/>
+      {/* <AboutPage/> */}
       {/* <UploadPage/> */}
-      {/* <HomePage /> */}
+      <HomePage />
     </div>
   );
 }
