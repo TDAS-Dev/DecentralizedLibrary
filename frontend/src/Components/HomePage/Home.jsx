b0fda3cc638a8fcc8fb82345e350a4beb9bd0fe7
Oghenetega Esedere <arcteggzz@gmail.com>
2022-05-16 16:32:18 +0100

    Created Home Page elements without styling

--- frontend/src/Components/HomePage/Home.jsx
+++ frontend/src/Components/HomePage/Home.jsx
@@ -1,4 +1,13 @@
+import ImageTest1 from "../../Images/Test01.jpg"
+import ImageTest2 from "../../Images/Test02.jpg"
+import ImageTest3 from "../../Images/Test03.png"
+import ImageTest4 from "../../Images/Test04.png"
+import ImageTest5 from "../../Images/Test05.jpg"
+
 function Home() {
+    const images = [
+        ImageTest1, ImageTest2, ImageTest3, ImageTest4, ImageTest5
+    ]
 
     const data = [
         {
@@ -12,16 +21,33 @@ function Home() {
             name : "Revenge of the heckles",
             link : "www.toubvoeejvbfevoofev.com",
             uploader : "0x6vew6wv7wve987wevvwvhdvjwdv8wewvS"
+        },
+        {
+            image : "",
+            name : "Back to Roots",
+            link : "www.infura.ceewcvev00dv.com",
+            uploader : "0x42nv2442v4v29vrrv8vfw82v2e0v2rvrv2r"
+        },
+        {
+            image : "",
+            name : "Heaven can wait",
+            link : "www.ipfs.vewnpweoudd88ddvdudv9ev22e.com",
+            uploader : "0x6vew6wvkjbhci086fgx546435esesszze3dv8wewvS"
+        },
+        {
+            image : "",
+            name : "Hoping for the Best",
+            link : "www.toubvoeejvbfevoofev.com",
+            uploader : "0x6vewxyytycxtx6689wevvwvhdvjwdv8wewvS"
         }
-
     ]
-
+                                                                                                                                              
     return (
-        <div classname="">
-            {data.map(elem => {
+        <div classname="flex flex-wrap lg:justify-center md:justify-center lg:w-[75%] md:w-[80%] sm:[90%]">
+            {data.map((elem, i) => {
                 return (
                     <div className="flex">
-                        <img src={elem.image} alt="" />
+                        <img src={images[i]} alt="" />
                         <h1>{elem.name}</h1>
                         <p>{elem.link}</p>
                         <p>{elem.uploader}</p>
