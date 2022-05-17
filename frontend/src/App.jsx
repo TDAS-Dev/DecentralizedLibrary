1a03972a613cd87e7837f42fe3203f0e335182e1
Oghenetega Esedere <arcteggzz@gmail.com>
2022-05-17 01:14:12 +0100

    Host on Netlify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import UploadPage from "./Components/UploadPage/UploadPage";
 import HomePage from "./Components/HomePage/HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import { Link } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
--- frontend/src/Components/AboutPage/About.jsx
+++ frontend/src/Components/AboutPage/About.jsx
@@ -1,46 +1,13 @@
-import Oghenetega from "../../Images/Oghenetega.png"
+import data from "./AboutData"
 
 function About(props){
-    const data = [
-        {
-            image: "../../Images/Oghenetega.png",
-            name: "Esedere Oghenetega Kelvin",
-            links : {
-                github: "",
-                twitter: "",
-                instagram: "",
-                linkedIn: "",
-                facebook:"",
-                portfolio: ""
-            },
-            description: "Oghenetega is an aspiring programmer in the Industry. He got into the industry in April, 2021. He is currently well versed in web development technologies as well as writing smart contracts using Solidity. His hobbies are problem solving, gaming and designing. A graduate of architecture from University of Lagos, Nigeria, he is open to all kinds of knowledge and learning.",
-            contact: {
-                email: "arcteggzz@gmail.com",
-                phone: "07088478400"
-            },
-            stack : "ReactJS, Solidity, Javascript",
-            industry: "Web Development, Blockchain, web3, web2"
-        },
-        {   
-            image: "../../Images/Oghenetega.png",
-            name: "Mike Doe ",
-            links : {
-                github: "",
-                twitter: "",
-                instagram: "",
-                linkedIn: "",
-                facebook:"",
-                portfolio: ""
-            },
-            description: "if it hurts to breathm this is a happy house, we're ahappy here, in the happy house oh this is fun funf unf this i sfun to nme. this is fun. Bring the 707 now. s is a happy house, we're ahappy here, in the happy house oh this is fun funf unf this i sfun to nme. this is fun. Bring the 707 now. s is a happy house, we're ahappy here, in the happy house oh this is fun funf unf this i sfun to nme. this is fun. Bring the 707 now",
-            contact: {
-                email: "abiolamike@gmail.com",
-                phone: "0704444444"
-            },
-            stack : "ReactJS, Solidity, Javascript, Golang",
-            industry: "Web Development, Blockchain, web3, web2"
-        }
-    ]
+    
+    function githubHandler(data) {
+        window.open(
+            `${data}`,
+            '_blank' // <- This is what makes it open in a new window.
+          )
+    }
 
     return(
         <div className="flex flex-col items-center">
@@ -49,10 +16,10 @@ function About(props){
                 The Project was a collaboration between two young and curious developers looking to explore the possibilities and applications of web3 and decentralization.
             </h2>
             <div className="my-6 sm:my-16 mb-10 lg:w-[75%] md:w-[88%] sm:w-[75%] flex flex-col lg:space-y-16 md:space-y-16 sm:space-y-16">
-                {data.map(elem => {
+                {data.map((elem, i) => {
                     return (
-                        <div className="flex lg:flex-row md:flex-row sm:flex-col lg:justify-between md:justify-between lg:items-center md:items-start sm:items-center">
-                            <img src={Oghenetega} alt="" className="lg:h-[250px] md:h-[190px] sm:h-[180px] lg:w-[250px] md:w-[190px] sm:w-[180px]"/>                            
+                        <div key={i} className="flex lg:flex-row md:flex-row sm:flex-col lg:justify-between md:justify-between lg:items-center md:items-start sm:items-center">
+                            <img src={elem.image} alt="" className="lg:h-[250px] md:h-[190px] sm:h-[180px] lg:w-[250px] md:w-[190px] sm:w-[180px]"/>                            
                             <div className="flex flex-col lg:items-start md:items-start sm:justify-center lg:max-w-[70%] md:max-w-[70%] sm:max-w-[100%]">
                                 <h1 className="text-black font-bold lg:text-2xl md:text-2xl sm:text-[18px] font-raleway sm:text-center">{elem.name}</h1>
                                 <p className="font-medium sm:text-[16px] sm:text-center">{elem.stack}</p>
@@ -62,7 +29,7 @@ function About(props){
                                 </div>
                                 <p className="text-black font-light lg:text-[16px] md:text-[16px] sm:text-[16px] lg:my-2 md:my-1 sm:my-2 font-lato text-warp lg:text-left md:text-left sm:text-center">{elem.description}</p>
                                 <div className="flex lg:flex-row md:flex-row sm:flex-col lg:justify-start md:justify-start sm:items-center lg:space-x-4 md:space-x-4 lg:space-y-0 md:space-y-0 sm:space-y-4 w-[100%]">
-                                    <button className={btnStyle}>Github</button>
+                                    <button className={btnStyle} onClick={()=> githubHandler(elem.links.github)} >Github</button>
                                     <button className={btnStyle}>Download CV</button>
                                     <button className={btnStyle}>Portfolio</button>                                    
                                 </div>                            
