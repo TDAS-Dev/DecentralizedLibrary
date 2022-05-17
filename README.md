7b093b6245e58ffd84f5ef584af3f6e0ce8b3a0c
Oghenetega Esedere <arcteggzz@gmail.com>
2022-05-17 11:09:20 +0100

    Sorted some LandingPage styling responsiveness

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 To Do List.
 
+1. Upload data objects (json files) to Ipfs and fetch them usnig fetch API.
+2. Upload images to ipfs and link using their url
+3. Reduce image sizes to reduce data costs
+4. Finish the deployment to Netlify.
 
 Project Team Page
 1. Abiola to fill in his details for the Project Team Page
@@ -13,8 +17,14 @@ Home Page
 2. Link the functionalities (On click of the Ipfs)
 3. Create the modal that will display the details of each card and add the necessary transitions and animations.
 4. Retrieve the data from the smart contract to display on screen. (get the array of uploaded cids)
+5. Add the private/public tag to each card.
 
 Upload Page
 1. Complete the styling of the page
 2. Link the functionality of uploading single files to the button.
+
+Landing Page
+1. Sort out the image background for the Library landing page. prevent the blurriness
+2. Link the explore Library button to the home page
+3. link the functionality of connect buttons to metamask using moralis sdk
 
--- frontend/src/Components/LandingPage/Hero.jsx
+++ frontend/src/Components/LandingPage/Hero.jsx
@@ -7,10 +7,10 @@ function Hero(props){
     return(
         <div className={heroPageStyle} style={heroStyle}>
             <LpNavBar/>
-            <div className="py-40">
+            <div className="lg:py-40 md:py-40 sm:pt-20 sm:pb-40 lg:flex lg:flex-col lg:items-center">
                 <h2 className={h2herostyle}>Welcome Back!</h2>
-                <h1 className={h1herostyle}>Unlimited books, Articles,</h1>
-                <h1 className={h1herostyle}>and more.</h1>
+                <h1 className={h1herostyle}>Unlimited books, Articles, and more.</h1>
+                {/* <h1 className={h1herostyle}></h1> */}
                 <h2 className={h2herostyle}>Read anytime. Access anywhere.</h2>
                 <div className={buttonDivStyle}>
                     <Button className="" name="Explore Library"/>
@@ -22,11 +22,11 @@ function Hero(props){
 }
 
 const heroPageStyle = `flex flex-col items-between min-h-screen text-center max-h-fit w-full max-h-100 h-full object-cover bg-no-repeat bg-cover bg-center`
-const h1herostyle = `text-white font-bold lg:text-6xl md:text-4xl sm:text-3xl text-center`
+const h1herostyle = `text-white font-bold lg:text-6xl md:text-4xl sm:text-3xl text-center lg:w-[60%]`
 const h2herostyle = `text-white lg:font-bold sm:font-semibold lg:text-2xl md:text-1xl sm:text-1xl text-center lg:my-6 md:my-4 sm:mt-2 sm:mb-3 font-lato tracking-wider`
 const heroStyle = {
     backgroundImage: `url(${heroImage})`
 }
-const buttonDivStyle = `flex lg:flex-row md:flex-row sm:flex-col lg:justify-center md:justify-center sm:items-center lg:items-center md:items-center lg:space-x-20 md:space-x-14 sm:space-y-7 lg:space-y-0 md:space-y-0`
+const buttonDivStyle = `flex lg:flex-row md:flex-row sm:flex-col lg:justify-center md:justify-center sm:items-center lg:items-center md:items-center lg:space-x-20 md:space-x-14 sm:space-y-7 lg:space-y-0 md:space-y-0 sm:mt-10`
 
 export default Hero;
