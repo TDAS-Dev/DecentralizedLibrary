7cec921777340c623a79aaf38063232a958479d6
Abiola Fasanya <hezekiahfasanya@gmail.com>
2022-05-28 20:14:32 +0000

    commit updated home page to use moralis setting

--- frontend/src/Components/HomePage/Container.jsx
+++ frontend/src/Components/HomePage/Container.jsx
@@ -2,5 +2,5 @@ const Container = (props) => {
   return <div className="flex flex-row space-x-5">{props.children}</div>;
 };
 
-const containerStyle = `grid grid-col-2 `
+// const containerStyle = `grid grid-col-2 `
 export default Container;
--- frontend/src/Components/HomePage/Nav.jsx
+++ frontend/src/Components/HomePage/Nav.jsx
@@ -1,7 +1,6 @@
-import {FaSearch, FaRecordVinyl} from "react-icons/fa"
-import {AiOutlineCloudDownload} from "react-icons/ai"
-import {BiStopCircle} from "react-icons/bi"
-
+import { FaSearch } from "react-icons/fa";
+import { AiOutlineCloudDownload } from "react-icons/ai";
+import { BiStopCircle } from "react-icons/bi";
 
 const Nav = () => {
   return (
@@ -9,26 +8,43 @@ const Nav = () => {
       <div className="w-[200px] text-2xl font-bold px-2">Library</div>
       <div className="flex flex-row justify-between items-center bg-gray-50 p-2 w-full">
         <div className="w-[500px] px-2 flex items-center space-x-2 bg-white mr-14">
-            <FaSearch className="text-gray-400 fa-solid" />
-          <input type="search" className="w-full rounded p-2 outline-none" placeholder="Search Library"/>
+          <FaSearch className="text-gray-400 fa-solid" />
+          <input
+            type="search"
+            className="w-full rounded p-2 outline-none"
+            placeholder="Search Library"
+          />
         </div>
         <div className="px-2">
-            <nav>
-                <ul className="flex space-x-4 items-center">
-                    <li><a href="" className="font-medium mr-2">Explore</a></li>
-                    <li><a href="" className="font-medium mr-2">Event</a></li>
-                    <li>
-                        <button className="bg-green-200 hover:bg-green-300 px-7 text-green-700 py-2 flex items-center">
-                        <BiStopCircle className="text-2xl mr-2 text-green-700"/>
-                        Record
-                        </button>
-                    </li>
-                    <li><button className="bg-green-500 hover:bg-green-600 px-7 text-green-50 py-2 flex items-center">
-                        <AiOutlineCloudDownload type="FaRegular" className="text-2xl mr-2 text-green-50"/>
-                      Upload
-                    </button></li>
-                </ul>
-            </nav>
+          <nav>
+            <ul className="flex space-x-4 items-center">
+              <li>
+                <a href="" className="font-medium mr-2">
+                  Explore
+                </a>
+              </li>
+              <li>
+                <a href="" className="font-medium mr-2">
+                  Event
+                </a>
+              </li>
+              <li>
+                <button className="bg-green-200 hover:bg-green-300 px-7 text-green-700 py-2 flex items-center">
+                  <BiStopCircle className="text-2xl mr-2 text-green-700" />
+                  Record
+                </button>
+              </li>
+              <li>
+                <button className="bg-green-500 hover:bg-green-600 px-7 text-green-50 py-2 flex items-center">
+                  <AiOutlineCloudDownload
+                    type="FaRegular"
+                    className="text-2xl mr-2 text-green-50"
+                  />
+                  Upload
+                </button>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
     </div>
